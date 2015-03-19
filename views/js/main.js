80b87b662bd4244af7747e37d8cd3880f8f1a993
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-03-19 07:59:00 -0500

    add onscreen jquery plugin to only udpate visible pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -502,7 +502,8 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.querySelectorAll('.mover').filter(":onScreen");
+  console.log(items.length);
   // TRIED TO ONLY UPDATE VISIBLE PIZZAS, BUT NOT WORKING YET
   // var visibleItems = []
   // for (var i = 0; i < items.length; i++) {
@@ -539,19 +540,19 @@ function updatePositions() {
 // runs updatePositions on scroll
 window.addEventListener('scroll', updatePositions);
 
-// // Generates the sliding pizzas when the page loads.
-// document.addEventListener('DOMContentLoaded', function() {
-//   var cols = 8;
-//   var s = 256;
-//   for (var i = 0; i < 200; i++) {
-//     var elem = document.createElement('img');
-//     elem.className = 'mover';
-//     elem.src = "images/pizza.png";
-//     elem.style.height = "100px";
-//     elem.style.width = "73.333px";
-//     elem.basicLeft = (i % cols) * s;
-//     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-//     document.querySelector("#movingPizzas1").appendChild(elem);
-//   }
-//   updatePositions();
-// });
+// Generates the sliding pizzas when the page loads.
+document.addEventListener('DOMContentLoaded', function() {
+  var cols = 8;
+  var s = 256;
+  for (var i = 0; i < 200; i++) {
+    var elem = document.createElement('img');
+    elem.className = 'mover';
+    elem.src = "images/pizza.png";
+    elem.style.height = "100px";
+    elem.style.width = "73.333px";
+    elem.basicLeft = (i % cols) * s;
+    elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    document.querySelector("#movingPizzas1").appendChild(elem);
+  }
+  updatePositions();
+});
