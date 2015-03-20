d6742c6b719fb4c38bae258afc976dd4b4f0af36
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-03-20 07:14:15 -0500

    add logging

--- views/js/main.js
+++ views/js/main.js
@@ -535,6 +535,7 @@ function updatePositions() {
   // for each items
   //   if item is visible
   //     push to visibleitems
+  console.log(document.body.scrollTop);
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
