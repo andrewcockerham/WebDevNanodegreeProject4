f74992462c790ce0c6aa606a7ec88114060f0684
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-03-20 15:40:09 -0500

    simplify phase 2

--- views/js/main.js
+++ views/js/main.js
@@ -540,10 +540,10 @@ function updatePositions() {
   //   if item is visible
   //     push to visibleitems
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin(5);
+    var phase = Math.sin(document.body.scrollTop / 1250));
     // var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-    // console.log(phase, document.body.scrollTop / 1250)
+    console.log(phase, document.body.scrollTop / 1250)
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
