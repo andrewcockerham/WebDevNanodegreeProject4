4e26c41769755537ccc0e46e14ee194cf8061164
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-03-20 15:53:59 -0500

    pull document.body.scrollTop out of for loop

--- views/js/main.js
+++ views/js/main.js
@@ -539,9 +539,10 @@ function updatePositions() {
   // for each items
   //   if item is visible
   //     push to visibleitems
+  var scrollTop = document.body.scrollTop / 1250
   for (var i = 0; i < items.length; i++) {
-    // var phase = Math.sin(Math.floor(i/5)*10);
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(scrollTop + (i % 5));
+    // var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     console.log(phase, Math.floor(i/5))
   }
