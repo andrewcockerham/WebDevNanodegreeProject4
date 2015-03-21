eb5ef15dca3e6b18a63bfed805598746070c074d
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-03-21 10:09:07 -0500

    move determineDx out of for loop 2

--- views/js/main.js
+++ views/js/main.js
@@ -453,13 +453,15 @@ var resizePizzas = function(size) {
     return dx;
   }
 
-  var pizzaContainers = document.getElementsByClassName("randomPizzaContainer");
-  var numPizzaContainers = pizzaContainers.length;
+
 
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    var dx = determineDx(pizzaContainers[i], size);
+    var pizzaContainers, numPizzaContainers, dx;
+    pizzaContainers  = document.getElementsByClassName("randomPizzaContainer");
+    numPizzaContainers  = pizzaContainers.length;
+    dx = determineDx(pizzaContainers[i], size);
     for (var i = 0; i < numPizzaContainers; i++) {
       var newwidth = (pizzaContainers[i].offsetWidth + dx) + 'px';
       pizzaContainers[i].style.width = newwidth;
