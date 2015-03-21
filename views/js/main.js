6e24ee5a7ae79c31951c2b9d3faee18860220a66
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-03-20 22:05:23 -0500

    back to originalasdfaasdfasdfasdfasadf

--- views/js/main.js
+++ views/js/main.js
@@ -542,7 +542,12 @@ function updatePositions() {
   var scrollTop = document.body.scrollTop / 1250
   // var arr = [0,1,2,3,4];
   for (var i = 0; i < items.length; i++) {
+    console.log("----");
+    console.log("i = " + i);
+    console.log("i % 5 = " + (i % 5));
+    console.log("document.body.scrollTop = " + document.body.scrollTop);
     var phase = Math.sin(scrollTop * i);
+    console.log("phase = " + phase);
     // var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     // console.log(phase, Math.sin(document.body.scrollTop / 1250));
@@ -565,7 +570,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 24; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
